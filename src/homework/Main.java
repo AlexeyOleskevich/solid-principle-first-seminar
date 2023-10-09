b0ea71a30b549899e8f50a903c31ef2194c4a7c8
AlexeyOleskevich <leshek2002@gmail.com>
2023-10-09 18:22:56 +0300

    Рестртуктуризировал проект.

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -3,7 +3,9 @@ package homework;
 public class Main{
     public static void main(String[] args){
         User user = new User("Bob");
-        user.report();
-        user.save();
+        UserReport userReport = new UserReport(user);
+        userReport.report();
+        Persister userPersister = new UserPersister(user);
+        userPersister.save();
     }
 }
